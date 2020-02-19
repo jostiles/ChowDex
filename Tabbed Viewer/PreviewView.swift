85ef32a5e7657522821b468e4119d3a62c18ee7c
Jordan Stiles <jordanstiles@Jordans-Mac-mini.local>
2020-02-18 22:33:07 -0600

    first commit

---
+++ Tabbed Viewer/PreviewView.swift
@@ -0,0 +1,9 @@
+//
+//  PreviewView.swift
+//  Tabbed Viewer
+//
+//  Created by Jordan Stiles on 2/14/20.
+//  Copyright © 2020 Jordan Stiles. All rights reserved.
+//
+
+import Foundation
---
+++ Tabbed Viewer/VisionViewController.swift
@@ -0,0 +1,9 @@
+//
+//  VisionViewController.swift
+//  Tabbed Viewer
+//
+//  Created by Jordan Stiles on 2/14/20.
+//  Copyright © 2020 Jordan Stiles. All rights reserved.
+//
+
+import Foundation
